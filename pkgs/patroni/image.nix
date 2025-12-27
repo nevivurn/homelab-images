8ebb4af004bd3782e79d05f22fb4cd802f6d146f
Yongun Seong <nevivurn@nevi.dev>
2025-12-27 23:50:27 +0900

    feat(patroni): shrink closure

--- pkgs/patroni/image.nix
+++ pkgs/patroni/image.nix
@@ -1,27 +1,58 @@
 {
   lib,
   dockerTools,
+  glibcLocales,
   tini,
   coreutils,
   patroni,
+  python3Packages,
   postgresql_18,
-  glibcLocales,
 }:
 
 let
+  patroni' = patroni.overridePythonAttrs {
+    dependencies = with python3Packages; [
+      # exclude dependencies for DCS modules other than etcd
+      click
+      prettytable
+      psutil
+      psycopg2
+      python-dateutil
+      python-etcd
+      pyyaml
+      tzlocal
+      urllib3
+      (ydiff.overridePythonAttrs {
+        # avoid including git, hg, etc. in the closure
+        patchPhase = ''
+          patchShebangs setup.py
+          patchShebangs tests/*.sh
+        '';
+        doCheck = false;
+      })
+    ];
+    disabledTestPaths = [
+      "tests/test_aws.py"
+      "tests/test_consul.py"
+      "tests/test_exhibitor.py"
+      "tests/test_raft.py"
+      "tests/test_raft_controller.py"
+      "tests/test_zookeeper.py"
+    ];
+  };
   locales = glibcLocales.override {
     allLocales = false;
   };
 in
 
 dockerTools.buildLayeredImage {
-  name = patroni.pname;
-  tag = patroni.version;
+  name = patroni'.pname;
+  tag = patroni'.version;
   config = {
     Entrypoint = [
       (lib.getExe tini)
       "--"
-      (lib.getExe' patroni "patroni")
+      (lib.getExe' patroni' "patroni")
     ];
     Env = [ "LOCALE_ARCHIVE=${locales}/lib/locale/locale-archive" ];
   };
@@ -29,7 +60,7 @@ dockerTools.buildLayeredImage {
     coreutils
     dockerTools.binSh
     dockerTools.fakeNss
-    patroni
+    patroni'
     postgresql_18
   ];
 }
